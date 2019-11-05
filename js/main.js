a01f76b9bc27b9d8163d298cf95eaf4c50561964
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-11-05 21:47:55 +0300

    +scrollup

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+
+$(document).ready(function () {    
+    $(function () {
+
+        $(window).scroll(function () {
+    
+            if ($(this).scrollTop() != 0) {
+    
+                $("#scrollUp").fadeIn();
+    
+            } else {
+    
+                $('#scrollUp').fadeOut();
+    
+            }
+    
+        });
+    
+        $('#scrollUp').click(function () {
+    
+            $('body,html').animate({ scrollTop: 0 }, 800);
+    
+        });
+    
+    });
+    
+});
